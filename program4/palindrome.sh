9e7ac39ab7fb2ce9bb0d1eb597bd4989e028ae5a
Miliya Rose Sephy <miliyarosesephy@gmail.com>
2024-12-19 12:07:06 +0530

    Add files via upload

---
+++ program4/palindrome.sh
@@ -0,0 +1,17 @@
+echo "enter the number "
+read n
+org=$n
+rev=0
+while [ $n -ne 0 ]
+do
+i=$(($n%10))
+rev=$((i + $rev * 10))
+n=$(($n/10))
+done
+if [ $org -eq $rev ]
+then
+echo " $org is a palindrome"
+else
+echo " $org is not a palindrome"
+fi
+
